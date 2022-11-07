60b4f8aac08cc95c8b26fce18c7e51f32c5bbef6
void <f@nullpt.rs>
2022-11-06 20:32:29 -0500

    refactor: eslint

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,13 +1,13 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
+import {GetStaticPaths, GetStaticProps} from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
-import { posts } from '../posts';
+import {posts} from '../posts';
 
 interface Props {
 	slug: string;
 }
 
-export default function PostPage({ slug }: Props) {
+export default function PostPage({slug}: Props) {
 	const post = posts.find(post => post.slug === slug)!;
 
 	return (
@@ -46,7 +46,7 @@ export default function PostPage({ slug }: Props) {
 	);
 }
 
-export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+export const getStaticProps: GetStaticProps<Props> = async ({params}) => {
 	const slug = params!.slug as string;
 
 	const post = posts.find(post => post.slug === slug);
@@ -65,6 +65,6 @@ export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
 };
 
 export const getStaticPaths: GetStaticPaths = async () => ({
-	paths: posts.map(post => ({ params: { slug: post.slug } })),
+	paths: posts.map(post => ({params: {slug: post.slug}})),
 	fallback: 'blocking',
 });
