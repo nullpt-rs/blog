5ff75d10fb7f0caa7ed79d2e4a5ca5467b2befc7
void <f@nullpt.rs>
2022-12-25 00:44:27 -0500

    Mnew blog post: Modernization of the Latrine

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -7,12 +7,14 @@ import {AnatomyOfASupremeBot2} from './2018/9/anatomy-of-a-supreme-bot-2/anatomy
 import {AnatomyOfASupremeBot3} from './2018/10/anatomy-of-a-supreme-bot-3/anatomy-of-a-supreme-bot-3';
 import {TacklingJavaScriptClientSideSecurity} from './2019/02/tackling-javascript-client-side-security/tackling-javascript-client-side-security-1';
 import {ReverseEngineeringTikTok} from './2022/12/reverse-engineering-tiktok-vm-obfuscation/reverse-engineering-tiktok';
+import {TheModernizationOfTheLatrine} from './2021/03/the-modernization-of-the-latrine/the-modernization-of-the-latrine';
 
 const ensurePosts = ensure<readonly Post[] | Post[]>();
 
 export const posts = ensurePosts([
   new ReverseEngineeringTikTok(),
   new NewBlog(),
+  new TheModernizationOfTheLatrine(),
   new TacklingJavaScriptClientSideSecurity(),
   new AnatomyOfASupremeBot3(),
   new AnatomyOfASupremeBot2(),
