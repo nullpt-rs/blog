33f9bde29fdc6ed8c2e7a701de1085afe1375d6a
void <f@nullpt.rs>
2023-01-12 21:47:12 -0500

    update rss feed

--- src/server/feed.ts
+++ src/server/feed.ts
@@ -7,8 +7,9 @@ import metadata4 from '../posts/2019/02/tackling-javascript-client-side-security
 import metadata5 from '../posts/2022/12/reverse-engineering-tiktok-vm-obfuscation/metadata.json'
 import metadata6 from '../posts/2022/05/new-blog/metadata.json'
 import metadata7 from '../posts/2023/01/devirtualizing-nike-bot-protection/metadata.json'
+import metadata8 from '../posts/2023/01/devirtualizing-nike-bot-protection-2/metadata.json'
 
-const posts = [metadata1, metadata2, metadata3, metadata4, metadata5, metadata6, metadata7];
+const posts = [metadata1, metadata2, metadata3, metadata4, metadata5, metadata6, metadata7, metadata8];
 
 export const feed = new Feed({
 	title: 'nullpt.rs • blog',
