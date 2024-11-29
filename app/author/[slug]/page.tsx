6d9bba04ee2780494620451e944238413f26a755
voidstar0 <25884226+voidstar0@users.noreply.github.com>
2024-11-29 15:44:06 -0500

    static authors

--- app/author/[slug]/page.tsx
+++ app/author/[slug]/page.tsx
@@ -1,9 +1,13 @@
 import { posts } from '../../utils/mdxUtils.const';
 import AuthorPage from './author-page';
 
-export const runtime = 'edge';
-
 export default async function Page({ params }: { params: { slug: string } }) {
 	const p = posts.filter(p => p.data.author === params!.slug);
 	return <AuthorPage author={params.slug} data={p} />;
+}
+
+export async function generateStaticParams() {
+	return posts.map(p => ({
+		slug: p.data.author,
+	}));
 }
