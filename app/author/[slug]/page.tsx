6eeb2932347a4d6a9c2185d94bb02c1289821336
voidstar0 <25884226+voidstar0@users.noreply.github.com>
2024-11-29 15:34:40 -0500

    fix ise

--- app/author/[slug]/page.tsx
+++ app/author/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import { posts } from '../../utils/mdxUtils.const';
 import AuthorPage from './author-page';
 
+export const runtime = 'edge';
+
 export default async function Page({ params }: { params: { slug: string } }) {
 	const p = posts.filter(p => p.data.author === params!.slug);
 	return <AuthorPage author={params.slug} data={p} />;
